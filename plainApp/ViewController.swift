d3d0330378022d4f6cbf7eb7c9a8a55aaa5df5ec
Scar Naruto <abdomuftah@hotmail.com>
2018-10-31 19:51:48 +0400

    Initial Commit

---
+++ plainApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  plainApp
+//
+//  Created by ScarNaruto on 10/31/18.
+//  Copyright © 2018 ScarNaruto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
